90230f455c482b1c7a931106b00361fba1b50642
YangWenlong123 <496192903@qq.com>
2023-09-06 15:58:05 +0800

    feat: 添加适配脚本

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,28 +3,18 @@
  * @Description: 页面入口
  * @Date: 2023-08-30 13:30:26
  * @LastEditors: along
- * @LastEditTime: 2023-09-06 15:55:03
+ * @LastEditTime: 2023-09-06 15:57:11
  * @FilePath: /cxy-food-menu/src/views/index.vue
 -->
 <template>
 	<van-button type="primary" class="button">主要按钮</van-button>
-	<van-button type="success">成功按钮</van-button>
-	<van-button type="default">默认按钮</van-button>
-	<van-button type="warning">警告按钮</van-button>
-	<van-button type="danger">危险按钮</van-button>
-
-	<van-radio-group v-model="checked">
-		<van-radio name="1">单选框 1</van-radio>
-		<van-radio name="2">单选框 2</van-radio>
-	</van-radio-group>
+	<div class="box">375 * 375</div>
 </template>
 
 <script lang="ts" setup>
 // import {useLoginStore} from '~/stores';
 // const {userInfo} = useLoginStore();
 import api from '~/api';
-
-const checked = ref('1');
 
 onMounted(() => {
 	api.login({account: '123'});
@@ -34,5 +24,12 @@ onMounted(() => {
 <style lang="scss" scoped>
 .button {
 	width: 10rem;
+	height: 1rem;
+}
+.box {
+	width: 5rem;
+	height: 5rem;
+	background: red;
+	font-size: 0.4rem;
 }
 </style>
